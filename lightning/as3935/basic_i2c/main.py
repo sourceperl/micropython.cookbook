7a412cfd39e62a1bb5c7979408cd27e69d9e659c
sourceperl <loic.celine@free.fr>
2022-08-16 18:48:21 +0200

    update lightning

--- lightning/as3935/basic_i2c/main.py
+++ lightning/as3935/basic_i2c/main.py
@@ -12,25 +12,51 @@ AS_I2C_ADDR = const(0X03)
 I2C_ID = const(0)
 I2C_SDA_PIN = const(0)
 I2C_SCL_PIN = const(1)
-# antenna tuning capacitance (must be integer multiple of 8, from 8 to 120 pf)
-AS_CAP_PF = const(120)
-AS_DISP_LCO = const(False)
-AS_DISP_SRCO = const(False)
-AS_DISP_TRCO = const(False)
+# AS special registers for commands
+AS_CMD_PRESET_DEFAULT = const(0x3c)
+AS_CMD_CALIB_RCO = const(0x3d)
+AS_CMD_TRIG_VAL = const(0x96)
+# AS registers address
+AS_REG_0 = const(0x00)
+AS_REG_1 = const(0x01)
+AS_REG_2 = const(0x02)
+AS_REG_3 = const(0x03)
+AS_REG_8 = const(0x08)
+# AS REG 0
+# AFE gain boost, default = 0b10010
 AS_AFE_GB = const(0b10010)
+# power down, default (active)
 AS_PWD = const(False)
+# AS REG 1
+# noise floor level, default = 0b010
 AS_NF_LEV = const(0b010)
-AS_WDTH = const(0b0100)
+# watchdog threshold, default = 0b0010
+AS_WDTH = const(0b0011)
+# AS REG 2
+# clear statistics, default = True
+AS_CL_STAT = const(True)
+# minimum number of lightning, default = 0b00
+AS_MIN_NUM_LIGHT = const(0b00)
+# spike rejection,default = 0b0010
+AS_SREJ = const(0b0010)
+# AS REG 3
+# interrupt mask values
 AS_INT_NOISE_HIGH = const(0x01)
 AS_INT_DIST_DETECT = const(0x04)
 AS_INT_LIGHTNING = const(0x08)
-AS_CMD_PRESET_DEFAULT = const(0x3c)
-AS_CMD_CALIB_RCO = const(0x3d)
-AS_CMD_TRIG = const(0x96)
-AS_REG_0 = const(0x00)
-AS_REG_1 = const(0x01)
-AS_REG_3 = const(0x03)
-AS_REG_8 = const(0x08)
+# REG 7
+# lightning distance
+AS_DIST_MASK = const(0x3f)
+AS_DIST_OUT = const(0x3f)
+# REG 8
+# display LCO on IRQ pin (default=False)
+AS_DISP_LCO = const(False)
+# display SRCO on IRQ pin (default=False)
+AS_DISP_SRCO = const(False)
+# display TRCO on IRQ pin (default=False)
+AS_DISP_TRCO = const(False)
+# antenna tuning capacitance (must be integer multiple of 8, from 8 to 120 pf)
+AS_CAP_PF = const(120)
 
 
 # some functions
@@ -64,29 +90,18 @@ i2c = I2C(I2C_ID, scl=Pin(I2C_SCL_PIN), sda=Pin(I2C_SDA_PIN), freq=400_000)
 
 # AS3935 send init command (see ams datasheet v1.04 page 23)
 # set all registers to default values
-i2c.writeto_mem(AS_I2C_ADDR, AS_CMD_PRESET_DEFAULT, bytes([AS_CMD_TRIG]))
-sleep_ms(2)
+i2c.writeto_mem(AS_I2C_ADDR, AS_CMD_PRESET_DEFAULT, bytes([AS_CMD_TRIG_VAL]))
 # calibrates automatically the internal RC oscillators
-i2c.writeto_mem(AS_I2C_ADDR, AS_CMD_CALIB_RCO, bytes([AS_CMD_TRIG]))
-sleep_ms(2)
-# set noise floor level and watchdog threshold
-# bits pattern: 0bRR-AFE_GB-PWD,
-# default is: 0bRR_10010_0
-reg_v = (AS_AFE_GB << 1) + AS_PWD
-i2c.writeto_mem(AS_I2C_ADDR, AS_REG_0, bytes([reg_v]))
-sleep_ms(2)
-# set noise floor level and watchdog threshold
-# bits pattern: 0bR-NF_LEV-WDTH
-# default is: 0bR_010_0010
-reg_v = (AS_NF_LEV << 4) + AS_WDTH
-i2c.writeto_mem(AS_I2C_ADDR, AS_REG_1, bytes([reg_v]))
-sleep_ms(2)
-# set internal tunning capacitor (from 0 to 120pF in steps of 8pF)
-# bits pattern: 0bDISP_LCO-DISP_SRCO-DISP_TRCO-R-TUN_CAP
-# default is: 0b0_0_0_R_0000
-reg_v = (AS_DISP_LCO << 7) + (AS_DISP_SRCO << 6) + (AS_DISP_TRCO << 5) + (AS_CAP_PF // 8)
-i2c.writeto_mem(AS_I2C_ADDR, AS_REG_8, bytes([reg_v]))
-sleep_ms(2)
+i2c.writeto_mem(AS_I2C_ADDR, AS_CMD_CALIB_RCO, bytes([AS_CMD_TRIG_VAL]))
+# customize default configuration with const values
+reg = (AS_AFE_GB << 1) + AS_PWD
+i2c.writeto_mem(AS_I2C_ADDR, AS_REG_0, bytes([reg]))
+reg = (AS_NF_LEV << 4) + AS_WDTH
+i2c.writeto_mem(AS_I2C_ADDR, AS_REG_1, bytes([reg]))
+reg = (0b1 << 7) + (AS_CL_STAT << 6) + (AS_MIN_NUM_LIGHT << 4) + AS_SREJ
+i2c.writeto_mem(AS_I2C_ADDR, AS_REG_2, bytes([reg]))
+reg = (AS_DISP_LCO << 7) + (AS_DISP_SRCO << 6) + (AS_DISP_TRCO << 5) + (AS_CAP_PF // 8)
+i2c.writeto_mem(AS_I2C_ADDR, AS_REG_8, bytes([reg]))
 
 # buzzer test
 print('test the buzzer at startup')
@@ -104,8 +119,13 @@ while True:
     as_int_status = i2c.readfrom_mem(AS_I2C_ADDR, AS_REG_3, 1)[0]
     if as_int_status:
         print(f'as_int_status = 0x{as_int_status:02x}')
-        as_dump_regs()
-        beep()
+        light_km = i2c.readfrom_mem(AS_I2C_ADDR, 0x07, 1)[0]
+        light_km &= AS_DIST_MASK
+        if light_km == AS_DIST_OUT:
+            log(f'distance is out of range')
+        else:
+            log(f'distance is {light_km} km')
+        # as_dump_regs()
     # process status on need
     if as_int_status & AS_INT_LIGHTNING:
         log('lightning occur')
@@ -114,10 +134,9 @@ while True:
         log(f'distance is {light_km} km')
         # get raw intensity
         energy_raw = int.from_bytes(i2c.readfrom_mem(AS_I2C_ADDR, 0x04, 3), 'little')
-        log(f'raw energy is {energy_raw / 2 ** 21:0}')
+        log(f'raw energy is {energy_raw / 2 ** 21:0.2f} %')
         # sound alert
-        # TODO set this
-        # beep()
+        beep()
     elif as_int_status & AS_INT_NOISE_HIGH:
         log('noise level too high')
     elif as_int_status & AS_INT_DIST_DETECT:
