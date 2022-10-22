ef75e447d6b94b114c1fcd983a9f16353acd327d
sourceperl <3809271+sourceperl@users.noreply.github.com>
2022-10-22 19:59:58 +0200

    minor fix

--- ads1x15/main.py
+++ ads1x15/main.py
@@ -20,7 +20,7 @@ while True:
     # read diff AINO - AIN1
     raw = adc.read(rate=1, channel1=0, channel2=1)
     # decode RAW value (gain=0 > +/-6_144mv > 1 bit = 0.1875mv)
-    volts = -1 * (raw * 0.1875 / 1000) * RES_RATIO
+    volts = RES_RATIO * -raw * 0.1875 / 1000
     # decode RAW value (gain=4 > +/-0_512mv > 1 bit = 0.015625mv)
     # volts = -1 * (raw * 0.015625 / 1000) * RES_RATIO
     print(f'value: {volts:0.3f} V (raw: {raw})')
